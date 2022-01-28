435c7f69c5f3015c31fbca03437a2bd313c1b453
Kamil Zegadlo <Kamil Zegadlo@DESKTOP-27UDJM0>
2022-01-28 12:13:21 +0100

    Add project files.

---
+++ ReactLearningWebApi.Tests/ProjectTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace ReactLearningWebApi.Tests
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ ReactLearningWebApi/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactLearningWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/project")]
+    public class ProjectController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddProject()
+        {
+            return View();
+        }
+    }
+}
