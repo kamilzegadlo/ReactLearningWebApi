c462f8d69705bfce0811bfe5f89ddc8651318c0f
Kamil Zegadlo <KamilZegadlo@gmail.com>
2022-01-30 23:26:07 +0100

    the very first implementation of the form to add a new project in react. Still need to learn unit tests in React.

---
+++ ReactLearningFrontEnd/src/NewProject.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+type NewProjectState = {
+    Name: string,
+    Description: string
+}
+
+class NewProject extends React.Component<any, NewProjectState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { Name: '', Description: '' };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleDescChange = this.handleDescChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleNameChange(event: any) {
+        this.setState({ Name: event.target.value });
+    }
+
+    handleDescChange(event: any) {
+        this.setState({ Description: event.target.value });
+    }
+
+    handleSubmit(event: any) {
+        alert('A name was submitted: ' + this.state.Name);
+        event.preventDefault();
+
+        let projectInfo = {
+            Name: this.state.Name,
+            Description: this.state.Description
+        }
+
+        fetch('https://localhost:7023/api/project', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(projectInfo),
+        }).then(r => r.json()).then(res => {
+            if (res) {
+                alert('Sent!');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <p>
+                    Adding a new project:
+                </p>
+                <label>
+                    Name:
+                    <input type="text" name="name" value={this.state.Name} onChange={this.handleNameChange} />
+                </label><br />
+                <label>
+                    Description:
+                    <textarea name="name" value={this.state.Description} onChange={this.handleDescChange} />
+                </label><br />
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default NewProject;
+
+
+
+
+
+
+
--- ReactLearningWebApi/Program.cs
+++ ReactLearningWebApi/Program.cs
@@ -5,15 +5,24 @@ using ReactLearningWebApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:3000").WithHeaders("Content-Type").WithMethods("POST");
+                      });
+});
 
 DIInstaller.RegisterServices(builder.Services);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
